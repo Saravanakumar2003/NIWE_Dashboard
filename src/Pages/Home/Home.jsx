c806c26a3644aee76a4984f436ae2680c4096c2e
Saravanakumar R <100985347+Saravanakumar2003@users.noreply.github.com>
2024-01-22 15:53:16 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,4 @@
-import React from "react"
-import { collection, query, onSnapshot } from "firebase/firestore";
-import { db } from "../../Firebase/firebaseConfig"
+import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Box from '@mui/material/Box';
@@ -8,12 +6,6 @@ import Button from '@mui/material/Button';
 import { Paper, Typography } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import Face6Icon from '@mui/icons-material/Face6';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import Grid from '@mui/material/Grid';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const Home = () => {
     let navigate = useNavigate();
@@ -21,9 +13,34 @@ const Home = () => {
     return (
         <>
             <Box sx={{ margin: "0 10px" }}>
+                <Typography variant="h2" sx={{ textAlign: "center", marginTop: 3 }}>
+                    Welcome to NIWE Dashboard - Developed for NIWE
+                </Typography>
             </Box>
-            <Paper elevation={5} sx={{ width: "95%", marginY: 4, marginX: "auto", padding: 0}}>
-                <Box sx={{ padding: 3, display: "flex", alignItems: "center", justifyContent: "center", gap: 3, flexFlow: "wrap row" }}>
+            <Paper elevation={5} sx={{ width: "95%", marginY: 4, marginX: "auto", padding: 3 }}>
+                <Typography variant="body1" sx={{ marginBottom: 3 }}>
+                    NIWE Dashboard is a powerful project management system developed during Saravanakumar's (Developer of this site) internship at the National Institute of Wind Energy (NIWE) for his mentor, Dr. K Boopathi Sir. It is tailor-made to meet the specific needs of Dr. Boopathi Sir and customized according to his requirements. The primary goal was to simplify and enhance project management, making it efficient for Dr. Boopathi Sir and his team at NIWE.
+                </Typography>
+
+                <Typography variant="body1" sx={{ marginBottom: 3 }}>
+                    <strong>Background:</strong> NIWE Dashboard was designed during my internship at NIWE, addressing the challenges faced by Dr. K Boopathi Sir in project management. The system is a testament to the commitment to making the work of people more manageable and efficient.
+                </Typography>
+
+                <Typography variant="body1" sx={{ marginBottom: 3 }}>
+                    <strong>Key Features:</strong>
+                    <ul>
+                        <li>Intuitive Project Dashboard</li>
+                        <li>Project Management and Tracking</li>
+                        <li>Employee Management and Tracking</li>
+                        <li>Attendance Management</li>
+                        <li>Export to Csv or print options </li>
+                        <li>Project and Employee Search</li>
+                        <li>Responsive Design</li>
+                        <li>And more...</li>
+                    </ul>
+                </Typography>
+
+                <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: 3, flexFlow: "wrap row" }}>
                     <Button
                         variant="contained"
                         color="success"
@@ -42,92 +59,9 @@ const Home = () => {
                         New User
                     </Button>
                 </Box>
-
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={4}>
-                    <SideNotification />
-
-                    </Grid>
-                    <Grid item xs={12} md={8}>
-                        <Box sx={{ padding: 4 }}>
-
-                            <Typography variant="h4" sx={{ marginBottom: 3, textDecoration: "underline", textAlign: "center" }}>
-                                Basic Guidelines
-                            </Typography>
-                            <ul>
-                                <li>
-                                    <Typography variant="body1" sx={{ marginBottom: 3 }}>
-
-                                    </Typography>
-                                </li>
-                                <li>
-                                    <Typography variant="body1" sx={{ marginBottom: 3 }}>
-
-                                    </Typography>
-                                </li>
-                                <li>
-                                    <Typography variant="body1" sx={{ marginBottom: 3 }}>
-
-                                    </Typography>
-                                </li>
-
-                            </ul>
-
-                        </Box>
-                    </Grid>
-
-                </Grid>
-
-
-            </Paper>
-
-
-        </>
-    )
-}
-export default Home
-const SideNotification = () => { // SideNotification
-    const [allNotifications, setAllNotifications] = React.useState([]) // allNotifications
-    React.useEffect(() => { 
-        const q = query(collection(db, "niwe_notification"));// query to get the user info from the database
-        const unsubscribe = onSnapshot(q, (querySnapshot) => { // onSnapshot
-            let tempNotific = [] // temporary variable to store the user info
-            querySnapshot.forEach((doc) => { // for each document in the querySnapshot
-                tempNotific.push({ ...doc.data(), key: doc.id }); // store the user info in mytempinfo
-            }); // end of forEach
-            setAllNotifications(tempNotific)// set the myinfo state to mytempinfo
-        }); // end of onSnapshot
-
-        return (() => { // return
-            unsubscribe() // unsubscribe from the query
-        })
-    }, [])
-    return (
-        <>
-            <Paper elevation={2} sx={{ margin: 2, bgcolor: "#008336" }}> 
-                <Typography variant="h6" sx={{ padding: 2, color: "white", display: 'flex', alignItems: "center", justifyContent: "center" }}>
-                    <NotificationsIcon sx={{ marginRight: 1 }} />Notification / Updates
-                </Typography>
-                <List component="marquee" direction="up" sx={{ width: '100%', height: "300px", bgcolor: 'background.paper' }}>
-                    {
-                        allNotifications.map((notific) => {// allNotifications
-                            return (
-                                <>
-                                    <ListItem>
-                                        <ListItemText
-                                            primary={notific.notification} // notification
-                                        />
-                                    </ListItem>
-                                    <Divider component="li" />
-                                </>
-                            )
-                        })
-                    }
-
-                </List>
             </Paper>
         </>
-    )
+    );
 }
 
-
+export default Home;
